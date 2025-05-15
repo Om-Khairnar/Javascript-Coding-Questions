42cb4157742867787fa515f3b243e1b843b2953e
Om Khairnar <143726540+Om-Khairnar@users.noreply.github.com>
2025-05-15 17:21:45 +0530

    Update reverseWords.js

--- reverseWords.js
+++ reverseWords.js
@@ -1,3 +1,5 @@
 function reverseWords(sentence) {
   return sentence.split(" ").reverse().join(" ");
 }
+console.log(reverseWords("I love coding")); // Output: "coding love I"
+console.log(reverseWords("Hello world!"));  // Output: "world! Hello"
